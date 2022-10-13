0e118f65acb854491cc3d26dcef1a0d383537a4d
Tim Allon <timallon@gmail.com>
2022-10-13 14:10:33 +0100

    iteration-2 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
+const canvas = document.querySelector('canvas');
+canvas.style.border = "2px solid blue";
+const ctx = canvas.getContext('2d');
+const startScreen = document.querySelector(".game-intro");
+
+const background = new Image ();
+background.src = "images/road.png";
+const car = new Image();
+car.src = "images/car.png"
+
+
 window.onload = () => {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(car, canvas.width * 0.412, canvas.height * 0.7, canvas.width * 0.17, canvas.height * 0.18);
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    startScreen.style.display = "none";
+  }
 };
