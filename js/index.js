da72b1824ad4711892a98bc8a4e05ec9f2f9ea38
Tim Allon <timallon@gmail.com>
2022-10-14 06:47:19 +0100

    iteration-3 not done

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const canvas = document.querySelector('canvas');
 canvas.style.border = "2px solid blue";
 const ctx = canvas.getContext('2d');
 const startScreen = document.querySelector(".game-intro");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+const carHorizontalSpeed = 3;
+
 
 const background = new Image ();
 background.src = "images/road.png";
@@ -10,8 +14,8 @@ car.src = "images/car.png"
 
 
 window.onload = () => {
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(car, canvas.width * 0.412, canvas.height * 0.7, canvas.width * 0.17, canvas.height * 0.18);
+  ctx.drawImage(background, 0, 0, canvasWidth, canvasHeight);
+  ctx.drawImage(car, canvasWidth * 0.421, canvasHeight * 0.7, canvas.width * 0.15, canvas.height * 0.18);
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
@@ -20,3 +24,11 @@ window.onload = () => {
     startScreen.style.display = "none";
   }
 };
+/* const speed, listen for event of button clicks, if right, move according to const, if left, go negative, if off, stop.*/
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
+  ctx.drawImage(car, x, y);                       // draw image at current position
+  x -= 4;
+  if (x > 250) requestAnimationFrame(animate)        // loop
+}
